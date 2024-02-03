0df4e92ee976d0e34a82db1cc9fc2d11e74e6b69
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-02-04 00:02:52 +0100

    feat: add greeting and goodbye messages

--- helpers/getUsername.js
+++ helpers/getUsername.js
@@ -1,4 +1,4 @@
 export function getUsername () {
   const username = process.argv.slice(2).find(arg => arg.startsWith('--username=')).split('=')[1];
-  console.log(`Welcome to the File Manager, ${username}!`)
+  return username
 }
--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-import {getUsername} from './username/username.js';
+import { getUsername } from './helpers/getUsername.js';
+import readline from 'readline';
 
-function app () {
-  getUsername();
+function app() {
+
+  const userName = getUsername();
+  console.log(`Welcome to the File Manager, ${userName}!`);
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> '
+  });
+
+  rl.on('line', (input) => {
+    if (input === '.exit') {
+      rl.close();
+    } else {
+      console.log('input:', input);
+      rl.prompt();
+    }
+  });
+
+  rl.on('close', () => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+  });
+
 }
 
-app()
+app();
